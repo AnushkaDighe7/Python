0703f57286de607cf96791cb253e9a8d6e37b67d
Anushka Dighe <120701208+AnushkaDighe7@users.noreply.github.com>
2023-09-23 15:50:13 +0530

    Add files via upload

---
+++ Function1.py
@@ -0,0 +1,13 @@
+
+# Function accepts nothing and return nothin
+def Marvellous():
+    print("Inside Marvellous Function")
+    
+
+
+def main():
+    Marvellous()
+
+
+if __name__ == "__main__":
+    main()    
---
+++ Function2.py
@@ -0,0 +1,14 @@
+# Function accepts one parameter and return nothin
+def Marvellous(Name):
+    print("Inside Marvellous Function")
+    print("Welcome",Name)
+    
+
+
+def main():
+    Marvellous("Amit")
+    Marvellous(90.90)
+
+
+if __name__ == "__main__":
+    main()    
